124f444aa2e0269c86b5493559c2ab513bf10acf
Cristian Pilapanta <capilapanta@uce.edu.ec>
2026-02-18 15:08:34 -0500

    feat: setup aws provider and network infrastructure

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_session_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "region" {
+  default = "us-east-1"
+}
